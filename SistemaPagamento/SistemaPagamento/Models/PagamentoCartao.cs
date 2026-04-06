862e4d61e88a70ddfc2caa3ad20b405de7f5ad6e
JulioZequin <163008238+JulioZequin@users.noreply.github.com>
2026-04-06 20:21:13 -0300

    Add files via upload

---
+++ SistemaPagamento/SistemaPagamento/Models/PagamentoCartao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPagamento.Models
+{
+    public class PagamentoCartao : Pagamento
+    {
+        public string NumeroCartao { get; set; }
+
+        public override string ProcessarPagamento()
+        {
+            return $"Processando pagamento de {Valor:C} via Cartão (Número: {NumeroCartao}) na data {Data:dd/MM/yyyy}.";
+        }
+    }
+}
