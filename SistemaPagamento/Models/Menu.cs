879b8a77fcf79b9fd28df394d3fd6de255d3fc0a
JulioZequin <163008238+JulioZequin@users.noreply.github.com>
2026-04-06 20:23:58 -0300

    Add files via upload

---
+++ SistemaPagamento/Models/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Menu
+{
+    public static void ExibirMenu()
+    {
+        Console.WriteLine("\n***** Sistema de Processamento de Pagamentos *****");
+        Console.WriteLine("********** Escolha a forma de pagamento **********");
+        Console.WriteLine("1 - Cartão");
+        Console.WriteLine("2 - Boleto");
+        Console.WriteLine("3 - Sair");
+        Console.Write("Escolha uma opção: ");
+    }
+}
